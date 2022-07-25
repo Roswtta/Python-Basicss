7c7991a6268a9381dd1fbcdfa725105fea275656
Rouzh Jaafar <103349444+Roswtta@users.noreply.github.com>
2022-07-25 18:10:38 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,134 @@
+# name = 'Zyzz'
+# age = 21
+# attractive = True
+
+# name, age, attractive = 'Zyzz', 21, True
+
+# print(name)
+# print(age)
+# print(attractive)
+
+# Spongebob = Patrick = Sandy = Squidward = 30
+
+# print(Spongebob)
+# print(Patrick)
+# print(Sandy)
+# print(Squidward)
+
+#--------------------------------------------------------
+
+# name = 'babyZyzz'
+
+# print(len(name))
+# print(name.find("a"))
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+# print(name.count("z"))
+# print(name.replace("z", "a"))
+# print(name*7)
+
+#--------------------------------------------------------
+
+# x = 1   #int
+# y = 2.0 #float
+# z = "3" #str
+
+# x = float(x)
+# y = int(y)
+# z = int(z)
+
+# x = str(x)
+
+# print("X is "+x)
+# print(y)
+# print(z)
+
+#--------------------------------------------------------
+
+# name = input("What is your name?: ")
+# age = int(input("How old are you?: "))
+# height = float(input("How tall are you?: "))
+
+#print("")
+# print("Hello " +name)
+# print("You are "+str(age)+" years old")
+# print("You are: "+str(height)+ "cm tall")
+
+#--------------------------------------------------------
+
+import math
+
+# pi = 3.14
+# x = 1
+# y = 2
+# z = 3
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(pow(pi, 2))
+# print(math.sqrt(pi))
+# print(max(x, y, z))
+# print(min(x, y, z))
+
+#--------------------------------------------------------
+
+# name = "Baby Zyzz"
+
+# first_name = name[0:5]
+# last_name = name[5:] # [5:9]
+# funky_name = name[0:9:2]
+
+# print(funky_name)
+
+#--------------------------------------------------------
+
+# website1 = "http://google.com"
+# website2 = "http://wikipedia.com"
+
+# slice = slice(7, -4)
+
+# print(website1[slice])
+# print(website2[slice])
+
+#--------------------------------------------------------
+
+# age = int(input("How old are you?: "))
+
+# if age == 100:
+    # print("You are a century old")
+# elif age >= 18:
+    # print("You are an adult")
+# elif age < 0:
+    # print("You haven't been born yet")
+# else:
+    # print("You are underage")
+
+#--------------------------------------------------------
+
+# temp = int(input("What is the temperature outside?: "))
+
+# if not (temp >= 0 and temp <= 30):
+    # print("The temperature is liveable")
+    # print("Go outside!")
+# elif temp <0 or temp >30:
+    # print("The temperature is unliveable today")
+    # print("Stay indoors")
+
+#--------------------------------------------------------
+
+# name = None
+
+# while not name:
+   # name = input("What's your name?: ")
+
+# print("Hello " + name)
+
+# while 1==1:
+    # name = print("I'm in a loop!")
+
+#--------------------------------------------------------
